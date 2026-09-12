04a61d00ec55ad79147d24170167ec97e0c23012
iamsagarsavita-cod <iamsagarsavita@gmail.com>
2026-09-12 23:56:40 +0530

    create a api for view profile, update, deleted

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -142,4 +142,132 @@ const login = async (req, res) => {
   }
 };
 
-module.exports = { signup, login };
+// Get My Profile
+const getMyProfile = async (req, res) => {
+  try {
+    let userId = req.userId;
+
+    let user = await UserModel.findById(userId).select("-password");
+    if (!user) {
+      return res.status(404).json({ msg: "User Not Found" });
+    }
+
+    return res.status(200).json({ msg: "Profile Fetched Successfully", user });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ msg: "Internal Server Error" });
+  }
+};
+
+// Update Profile
+const updateProfile = async (req, res) => {
+  try {
+    let userId = req.userId;
+    let userData = req.body;
+
+    if (!userData || Object.keys(userData).length === 0) {
+      return res
+        .status(400)
+        .json({ msg: "Bad Request ! Enter Data to Update" });
+    }
+
+    let { fullName, email, password, phone, bio } = userData;
+
+    if (fullName !== undefined) {
+      if (!isValid(fullName)) {
+        return res.status(400).json({ msg: "Full Name is Required" });
+      }
+
+      if (!isValidFullName(fullName)) {
+        return res.status(400).json({ msg: "Invalid Fullname" });
+      }
+    }
+
+    if (email !== undefined) {
+      if (!isValid(email)) {
+        return res.status(400).json({ msg: "Email is Required" });
+      }
+
+      if (!isValidEmail(email)) {
+        return res.status(400).json({ msg: "Invalid Email" });
+      }
+
+      let duplicateEmail = await UserModel.findOne({
+        email,
+        _id: { $ne: userId },
+      });
+      if (duplicateEmail) {
+        return res.status(400).json({ msg: "Email Already Exists" });
+      }
+    }
+
+    if (password !== undefined) {
+      if (!isValid(password)) {
+        return res.status(400).json({ msg: "Password is Required" });
+      }
+
+      if (!isValidPassword(password)) {
+        return res.status(400).json({ msg: "Invalid Password" });
+      }
+      const hashedPassword = await bcrypt.hash(password, 10);
+      userData.password = hashedPassword;
+    }
+
+    if (phone !== undefined) {
+      if (!isValid(phone)) {
+        return res.status(400).json({ msg: "Phone Number is Required" });
+      }
+
+      if (!isValidPhone(phone)) {
+        return res.status(400).json({ msg: "Invalid Phone Number" });
+      }
+
+      let duplicatePhone = await UserModel.findOne({
+        phone,
+        _id: { $ne: userId },
+      });
+      if (duplicatePhone) {
+        return res.status(400).json({ msg: "Phone Number Already Exists" });
+      }
+    }
+
+    if (bio !== undefined) {
+      if (bio.length < 15 && bio.length > 200) {
+        return res
+          .status(400)
+          .json({ msg: "Bio Cannot be less than 15 Characters." });
+      }
+    }
+
+    let updatedUserProfile = await UserModel.findByIdAndUpdate(
+      userId,
+      userData,
+      { new: true },
+    ).select("-password");
+
+    return res
+      .status(200)
+      .json({ msg: "Profile Updated Successfully", updatedUserProfile });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ msg: "Internal Server Error" });
+  }
+};
+
+// Delete Profile
+const deleteProfile = async (req, res) => {
+  try {
+    let userId = req.userId;
+
+    let deletedUser = await UserModel.findByIdAndDelete(userId);
+    if (!deletedUser) {
+      return res.status(404).json({ msg: "User Not Found or Already Deleted" });
+    }
+    return res.status(200).json({ msg: "Profile Deleted Successfully" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ msg: "Internal Server Error" });
+  }
+};
+
+module.exports = { signup, login, getMyProfile, updateProfile, deleteProfile };
