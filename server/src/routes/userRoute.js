b550db5c600cbdd32c10c01aa10aba886b4f62d4
iamsagarsavita-cod <iamsagarsavita@gmail.com>
2026-09-16 23:57:01 +0530

    solve the error

--- server/src/routes/userRoute.js
+++ server/src/routes/userRoute.js
@@ -12,7 +12,7 @@ const { authentication, authorization } = require("../middlewares/auth");
 router.post("/signup", signup);
 router.post("/login", login);
 router.get("/my-profile", authentication, getMyProfile);
-router.put("update", authentication, updateProfile);
+router.put("/update", authentication, updateProfile);
 router.delete("/delete", authentication, deleteProfile);
 
 module.exports = router;
